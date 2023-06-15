41817be69829d559ba1fe818983b6505fe99bace
hoangatuan <tuanhanh19981998@gmail.com>
2023-06-15 23:33:39 +0700

    Init project

---
+++ iMovie/iMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  iMovieApp.swift
+//  iMovie
+//
+//  Created by Tuan Hoang on 15/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct iMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
