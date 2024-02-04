f75ca58c6871d02e998df224be554b59aafb7964
Hoang Anh Tuan <tuanhanh19981998@gmail.com>
2024-02-04 17:13:09 +0800

    Integrate tusit

---
+++ Project.swift
@@ -0,0 +1,213 @@
+import ProjectDescription
+
+let project = Project(
+    name: "iMovie",
+    organizationName: nil,
+    packages: [
+        .package(url: "https://github.com/apple/swift-log.git", from: "1.2.0"),
+        .package(url: "https://github.com/apple/swift-syntax.git", from: "509.0.0"),
+        .package(url: "https://github.com/AliSoftware/OHHTTPStubs", from: "9.1.0"),
+        .package(url: "https://github.com/kean/Nuke", from: "12.0.0"),
+        .package(url: "https://github.com/markiv/SwiftUI-Shimmer", .exact("1.1.0")),
+        .package(path: .relativeToRoot("Foundation/HelperMacros")),
+    ],
+    settings: nil,
+    targets: [
+        Target(
+            name: "Logger",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Logger",
+            deploymentTargets: .iOS("13.0"),
+            sources: ["Foundation/Logger/Sources/Logger/**"],
+            resources: nil,
+            dependencies: [
+                .package(product: "Logging", type: .runtime, condition: nil),
+            ]
+        ),
+        Target(
+            name: "Router",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Router",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Foundation/Router/Sources/Router/**"],
+            resources: nil,
+            dependencies: []
+        ),
+//        Target(
+//            name: "HelperMacroMacros",
+//            destinations: Set(Array(ProjectDescription.Destination.allCases)),
+//            product: .macro,
+//            bundleId: "io.tuist.HelperMacroMacros",
+//            deploymentTargets: .iOS("13.0"),
+//            sources:  ["Foundation/HelperMacros/Sources/HelperMacrosMacros/**"],
+//            resources: nil,
+//            dependencies: [
+//                .package(product: "SwiftSyntaxMacros", type: .runtime),
+//                .package(product: "SwiftCompilerPlugin", type: .runtime),
+//            ]
+//        ),
+//        Target(
+//            name: "HelperMacros",
+//            destinations: Set(Array(ProjectDescription.Destination.allCases)),
+//            product: .macro,
+//            bundleId: "io.tuist.HelperMacros",
+//            deploymentTargets: .iOS("16.0"),
+//            sources: ["Foundation/HelperMacros/Sources/HelperMacros/**"],
+//            resources: nil,
+//            dependencies: [
+//                .target(name: "HelperMacroMacros")
+//            ]
+//        ),
+        Target(
+            name: "DesignSystem",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.DesignSystem",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Foundation/DesignSystem/Sources/DesignSystem/**"],
+            resources: nil,
+            dependencies: []
+        ),
+        Target(
+            name: "Network",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Network",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Foundation/Network/Sources/Network/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Logger"),
+            ]
+        ),
+        Target(
+            name: "Domain",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Domain",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Foundation/Domain/Sources/Domain/**"],
+            resources: nil,
+            dependencies: [
+                .package(product: "HelperMacros", type: .macro),
+            ]
+        ),
+        Target(
+            name: "DomainData",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.DomainData",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Foundation/Domain/Sources/DomainData/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .target(name: "Network"),
+            ]
+        ),
+        Target(
+            name: "CommonUI",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.CommonUI",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Core/CommonUI/Sources/CommonUI/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .package(product: "Shimmer", type: .runtime, condition: nil),
+                .package(product: "NukeUI", type: .runtime, condition: nil),
+            ]
+        ),
+        Target(
+            name: "People",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.People",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Features/People/Sources/People/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .target(name: "Network"),
+                .target(name: "CommonUI"),
+                .target(name: "DomainData"),
+                .target(name: "Router"),
+                .target(name: "DesignSystem"),
+            ]
+        ),
+        Target(
+            name: "TVSeries",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.TVSeries",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Features/TVSeries/Sources/TVSeries/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .target(name: "Network"),
+                .target(name: "CommonUI"),
+                .target(name: "DomainData"),
+                .target(name: "Router"),
+                .target(name: "DesignSystem"),
+            ]
+        ),
+        Target(
+            name: "Movies",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Movies",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Features/Movies/Sources/Movies/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .target(name: "Network"),
+                .target(name: "CommonUI"),
+                .target(name: "DomainData"),
+                .target(name: "Router"),
+                .target(name: "DesignSystem"),
+            ]
+        ),
+        Target(
+            name: "Search",
+            destinations: .iOS,
+            product: .framework,
+            bundleId: "io.tuist.Search",
+            deploymentTargets: .iOS("16.0"),
+            sources: ["Features/Search/Sources/Search/**"],
+            resources: nil,
+            dependencies: [
+                .target(name: "Domain"),
+                .target(name: "Network"),
+                .target(name: "CommonUI"),
+                .target(name: "DomainData"),
+                .target(name: "Router"),
+                .target(name: "DesignSystem"),
+            ]
+        ),
+        Target(
+            name: "iMovie",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "io.tuist.iMovie",
+            deploymentTargets: .iOS("16.0"),
+            sources: [
+                "iMovie/Application/**",
+                "iMovie/Presentation/**",
+            ],
+            resources: [
+                "iMovie/Resources/**",
+            ],
+            dependencies: [
+                .target(name: "People"),
+                .target(name: "Movies"),
+                .target(name: "Search"),
+                .target(name: "TVSeries"),
+            ]
+        ),
+    ]
+)
