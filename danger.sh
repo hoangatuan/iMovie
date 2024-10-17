7690be545454220711b22fe11116bbd4f74227f1
tuanhoang <tuanhanh19981998@gmail.com>
2024-10-18 00:03:50 +0800

    feat: Update script

--- danger.sh
+++ danger.sh
@@ -1,11 +1,6 @@
 #! /bin/bash
 
-if [ -z "$CI" ]; then
-    echo "Not running in a CI environment. Exiting."
-    exit 0
-fi
-
- echo "Start Danger!!!"
+echo "Start Danger!!!"
 
 export DANGER_GITHUB_API_TOKEN="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"
 
@@ -14,4 +9,4 @@ INDEX_STORE_PATH=$DERIVED_DATA_PATH/Index.noindex/DataStore
 export INDEX_STORE_PATH
 
 cd ./DangerSwiftPeriphery
-swift run danger-swift local --danger-js-path ./danger-arm64
+swift run danger-swift ci
